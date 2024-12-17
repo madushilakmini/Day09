960424f94d9992295be96626bd2cace838c23aa4
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2024-12-17 21:11:42 +0530

    Add styles for logo, text, and subtitle

--- firstapp/app/index.tsx
+++ firstapp/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View,Image } from "react-native";
+import { Text, View,Image,StyleSheet } from "react-native";
 
 export default function Index() {
   return (
@@ -7,11 +7,34 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#008080",
       }}
     >
-      <Image source={require("../assets/images/react-logo.png")}/>
-      <Text>Hello World!</Text>
-      <Text>My first ReactNative cross platform app</Text>
+      <Image source={require("../assets/images/react-logo.png")} style={styles.logo}/>
+      <Text style={styles.txt}>Hello World!</Text>
+      <Text style={styles.subtxt}>My first ReactNative cross platform app</Text>
     </View>
   );
 }
+const styles= StyleSheet.create({
+  logo:{
+    width:120,
+    height:120,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: 'lightblue',
+    marginBottom: 20,
+  },
+  txt:{
+    fontSize:32,
+    color:"#4B0082",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 10,
+  },
+  subtxt:{
+    fontSize: 18, 
+    color: '#8B4513',
+    textAlign: 'center',
+  },
+})
